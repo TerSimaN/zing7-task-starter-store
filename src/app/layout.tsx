6770bbe9eacbe961be2748207e86fe790fe456a0
TerSimaN <mcsiman998@gmail.com>
2024-06-23 20:13:33 +0300

    Refactored code, added custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import { Metadata } from "next"
+import { Rubik, Roboto } from "next/font/google"
 import "styles/globals.css"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  variable: "--font-rubik"
+});
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto"
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
@@ -10,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-      <body>
+      <body className={`${rubik.variable} ${roboto.variable}`}>
         <main className="relative">{props.children}</main>
       </body>
     </html>
